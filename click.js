19bbb0fc53cccca28b4bbb1595a0c7304425f7f3
presigues <132411345+presigues@users.noreply.github.com>
2024-07-17 17:47:07 -0400

    debug rotation

--- click.js
+++ click.js
@@ -1,12 +1,53 @@
-var rt;
-window.addEventListener('keydown', (event) =>{
+var rt = 0;
+
+var time = 0;
+window.addEventListener('keypress', (event) =>{
     console.log(event)
     
     switch(event.key){
-      case 'e': // jump 
+      case 'd': // jump 
         console.log("e")
-        document.getElementById('model1').setAttribute('rotation', {x: 0, y: 90, z: 0});
-        document.getElementById('model1').setAttribute('position', {x: -.5, y: 0, z: 0});
+        
+        document.getElementById('model1').setAttribute('rotation', {x: 0, y: rt = rt + 90, z: 0});
+
+        time = time + 1;
+
+        if(time == 1){
+        document.getElementById('model1').setAttribute('position', {x: 0, y: .4, z: -1});
+
+        time = time + 1;
+}
+        if(time == 2){
+        
+        document.getElementById('model1').setAttribute('position', {x: -.4, y: 0, z: -1});
+        time = time +1;
+        }
+
+        if(time == 3){
+        
+            document.getElementById('model1').setAttribute('position', {x: .8, y: 0, z: -2});
+            time = time +1;
+            }
+
+            if(time == 4){
+        
+                document.getElementById('model1').setAttribute('position', {x: -.4, y: 0, z: -1});
+                time = time + 1 ;
+                }
+
+                if(time == 5){
+        
+                    document.getElementById('model1').setAttribute('position', {x: 0, y: 0, z: -1});
+                    time = time - 5;
+                    }
+    
+        break;
+
+        case 'a': // jump 
+        console.log("e")
+        
+        document.getElementById('model1').setAttribute('rotation', {x: 0, y: rt = rt + 90, z: 0});
+        document.getElementById('model1').setAttribute('position', {x: -.4, y: 0, z: -1});
     
         break;
 
