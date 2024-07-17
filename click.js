012abd7a28c2bb8a30d232e94795ca3c69174566
presigues <132411345+presigues@users.noreply.github.com>
2024-07-17 15:27:01 -0400

    adding rotation button to model

--- click.js
+++ click.js
@@ -1,18 +1,16 @@
-
+var rt;
 window.addEventListener('keydown', (event) =>{
     console.log(event)
     
     switch(event.key){
       case 'e': // jump 
         console.log("e")
-        document.getElementById('model1').style.material = "opacity: 0.0; transparent: true";
+        document.getElementById('model1').setAttribute('rotation', {x: 0, y: 90, z: 0});
+        document.getElementById('model1').setAttribute('position', {x: -.5, y: 0, z: 0});
     
-        break
-        case 'q': // left
-    
-    document.getElementById('model1').style.material = "opacity: 1; transparent: false";
-        
+        break;
 
     }
-    })
+
+    });
     
